bb467d15bd0e36ef9ea2f0b1971814c25ecd7720
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-12 18:04:46 +0900

    ✨ Solve : 15649

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UncheckedIOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 interface ISupplier extends IStructModifier, IStructState {
@@ -49,15 +52,18 @@ public class Main {
 }
 
 interface IStructModifier {
+    public void push(QInput input);
 }
 
 interface IStructState {
+    public int getRange();
 }
 
 class Supplier implements ISupplier {
     final BufferedReader reader;
-    private final Iterator<Integer> numbIterator;
-    private final QInput _cache;
+    final int range;
+    final int nLimit;
+    private final Deque<QInput> deque = new ArrayDeque<>();
 
     private List<Integer> readMultiLineIntegers() {
         try {
@@ -74,38 +80,79 @@ class Supplier implements ISupplier {
         this.reader = br;
         try {
             List<Integer> numbers = readMultiLineIntegers();
-            this.numbIterator = numbers.iterator();
-            int max = numbers.stream().max(Integer::compareTo).orElse(numbers.get(0));
-            _cache = new QInput(max, numbers.get(0));
+            this.range = numbers.get(0);
+            this.nLimit = numbers.get(1);
         } catch (Exception e) {
             throw new IllegalArgumentException();
+        }
+
+        for (int initialNumber = 1; initialNumber <= range; initialNumber++) {
+            if (nLimit == 1) {
+                System.out.println(initialNumber);
+                continue;
+            }
+            QInput input = new QInput(nLimit, initialNumber);
+            deque.offer(input);
         }
     }
 
     @Override
     public QInput next() {
-        int n = numbIterator.next();
-        _cache.set(n);
-        return _cache;
+        return deque.poll();
     }
 
     @Override
     public boolean hasNext() {
-        return numbIterator.hasNext();
+        return !deque.isEmpty();
+    }
+
+    @Override
+    public void push(QInput input) {
+        deque.offer(input);
+    }
+
+    @Override
+    public int getRange() {
+        return range;
     }
 }
 
 class QInput {
-    final int max;
-    int n;
+    final int nLimit;
+    final private List<Integer> numbers = new ArrayList<>();
+    final private Map<Integer, Boolean> isChosen = new HashMap<>();
 
-    public QInput(int max, int n) {
-        this.max = max;
-        this.n = n;
+    public QInput(int nLimit, int first) {
+        this.nLimit = nLimit;
+        this.numbers.add(first);
+        this.isChosen.put(first, true);
     }
 
-    public void set(int n) {
-        this.n = n;
+    public boolean isChosen(int number) {
+        return isChosen.getOrDefault(number, false);
+    }
+
+    public void choose(int number) {
+        numbers.add(number);
+        isChosen.put(number, true);
+    }
+
+    public boolean isComplete() {
+        return numbers.size() == nLimit;
+    }
+
+    public void print(StringBuilder sb) {
+        sb.append(this.numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "))).append("\n");
+    }
+
+    public QInput copyOf() {
+        QInput copy = new QInput(nLimit, numbers.get(0));
+        for (int i = 1; i < numbers.size(); i++) {
+            copy.choose(numbers.get(i));
+        }
+        return copy;
     }
 }
 
@@ -114,30 +161,27 @@ class Consumer implements IConsumer {
     final ISupplier supplier;
     final StringBuilder sb = new StringBuilder();
 
-    final List<String> dp = new ArrayList<>(Arrays.asList("-", "- -", "- -   - -"));
-
     public Consumer(BufferedWriter bw, ISupplier supplier) {
         this.writer = bw;
         this.supplier = supplier;
     }
 
-    private void initDp(int _max) {
-        int i = dp.size();
-        StringBuilder sb = new StringBuilder();
-        sb.append(dp.get(i - 1));
-        while (i <= _max) {
-            // 3 ** (3-1)
-            sb.append(" ".repeat((int) Math.pow(3, i - 1)));
-            sb.append(dp.get(i - 1));
-            dp.add(sb.toString());
-            i++;
-        }
-    }
-
     @Override
     public void consume(QInput input) throws IOException {
-        initDp(input.max);
-        sb.append(dp.get(input.n)).append("\n");
+        for (int i = 1; i <= supplier.getRange(); i++) {
+            if (input.isChosen(i)) {
+                continue;
+            }
+
+            QInput newInput = input.copyOf();
+            newInput.choose(i);
+
+            if (newInput.isComplete()) {
+                newInput.print(sb);
+            } else {
+                supplier.push(newInput);
+            }
+        }
     }
 
     @Override
