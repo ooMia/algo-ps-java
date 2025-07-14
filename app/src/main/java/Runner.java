265588a7d3e59aa72339944f460a2ca7858ad2ee
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-14 23:52:25 +0900

    ✨ Solve : 1260

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,36 +1,41 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int T, maxT = 40;
-    final int[] fibo0 = new int[maxT + 1];
-    final int[] fibo1 = new int[maxT + 1];
-    final List<Integer> numbers;
-
-    int maxN = -1;
+    final int N, M, V;
+    final Map<Integer, Set<Integer>> graph = new HashMap<>();
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
 
         try {
-            this.T = reader.readInts()[0];
-            fibo0[0] = 1;
-            fibo1[1] = 1;
+            var _vnm = reader.readInts();
+            this.N = _vnm[0];
+            this.M = _vnm[1];
+            this.V = _vnm[2];
+            reader.lines().forEach(line -> {
+                var parts = line.split(" ");
+                int source = Integer.parseInt(parts[0]);
+                int dest = Integer.parseInt(parts[1]);
+                graph.computeIfAbsent(source, k -> new TreeSet<>()).add(dest);
+                graph.computeIfAbsent(dest, k -> new TreeSet<>()).add(source);
+            });
 
-            numbers = reader.lines().stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-            {
-                var l = new ArrayList<>(numbers);
-                l.sort(Integer::compareTo);
-                maxN = l.get(l.size() - 1);
+            for (Integer key : graph.keySet()) {
+                System.err.println("Key: " + key + ", Value: " + graph.get(key));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -49,12 +54,51 @@ class Runner implements IRunner {
 
     @Override
     public void run() {
-        for (int i = 2; i <= maxN; i++) {
-            fibo0[i] = fibo0[i - 1] + fibo0[i - 2];
-            fibo1[i] = fibo1[i - 1] + fibo1[i - 2];
+        {
+            boolean[] visited = new boolean[N + 1];
+            dfs(V, visited);
         }
-        for (Integer n : numbers) {
-            sb.append(fibo0[n]).append(' ').append(fibo1[n]).append('\n');
+        sb.append("\n");
+        {
+            Queue<Integer> queue = new ArrayDeque<>();
+            boolean[] visited = new boolean[N + 1];
+            queue.add(V);
+
+            while (!queue.isEmpty()) {
+                int v = queue.poll();
+                if (visited[v])
+                    continue;
+                bfs(v, visited, queue);
+            }
+        }
+    }
+
+    private void dfs(int v, boolean[] visited) {
+        visited[v] = true;
+        sb.append(v).append(" ");
+
+        Iterator<Integer> neighbors = graph.getOrDefault(v, new TreeSet<>()).iterator();
+        if (!neighbors.hasNext()) {
+            return;
+        }
+        while (neighbors.hasNext()) {
+            int neighbor = neighbors.next();
+            if (!visited[neighbor]) {
+                dfs(neighbor, visited);
+            }
+        }
+    }
+
+    private void bfs(int v, boolean[] visited, Queue<Integer> queue) {
+        visited[v] = true;
+        sb.append(v).append(" ");
+
+        Iterator<Integer> neighbors = graph.getOrDefault(v, new TreeSet<>()).iterator();
+        if (!neighbors.hasNext()) {
+            return;
+        }
+        while (neighbors.hasNext()) {
+            queue.add(neighbors.next());
         }
     }
 
