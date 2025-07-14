829c33bedb0d2ef81cd53f92fa87bb81bbd9575d
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-15 00:35:59 +0900

    ✨ Solve : 2606

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,24 +1,36 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int M;
-    final Data input = new Data();
-    final java.util.Set<Integer> set = new java.util.HashSet<>();
-    final java.util.List<Integer> allSet = java.util.stream.IntStream.range(1, 21).boxed()
-            .collect(java.util.stream.Collectors.toList());
+    final int N, V = 1;
+    final Map<Integer, Set<Integer>> graph = new HashMap<>();
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
 
         try {
-            this.M = reader.readInts()[0];
+            this.N = reader.readInts()[0];
+            reader.skipLine();
+            reader.lines().forEach(line -> {
+                var parts = line.split(" ");
+                int source = Integer.parseInt(parts[0]);
+                int dest = Integer.parseInt(parts[1]);
+                graph.computeIfAbsent(source, k -> new HashSet<>()).add(dest);
+                graph.computeIfAbsent(dest, k -> new HashSet<>()).add(source);
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -35,59 +47,44 @@ class Runner implements IRunner {
     }
 
     @Override
-    public void run() throws IOException {
-        for (int i = 0; i < M; ++i) {
-            input.set(reader.line());
-            consume(input);
+    public void run() {
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[N + 1];
+        queue.add(V);
+
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            if (visited[v])
+                continue;
+            bfs(v, visited, queue);
         }
+
+        int count = -1;
+        for (int i = 1; i <= N; ++i) {
+            if (visited[i]) {
+                ++count;
+            }
+        }
+        sb.append(count).append("\n");
     }
 
-    private void consume(Data data) {
-        switch (data.op) {
-            case add:
-                set.add(data.operand);
-                break;
-            case remove:
-                set.remove(data.operand);
-                break;
-            case check:
-                sb.append(set.contains(data.operand) ? 1 : 0).append("\n");
-                break;
-            case toggle:
-                if (set.contains(data.operand)) {
-                    set.remove(data.operand);
-                } else {
-                    set.add(data.operand);
-                }
-                break;
-            case all:
-                set.addAll(allSet);
-                break;
-            case empty:
-                set.clear();
-                break;
+    private void bfs(int v, boolean[] visited, Queue<Integer> queue) {
+        visited[v] = true;
+        System.err.println("Visiting: " + v);
+
+        Iterator<Integer> neighbors = graph.getOrDefault(v, new HashSet<>()).iterator();
+        if (!neighbors.hasNext()) {
+            return;
+        }
+        while (neighbors.hasNext()) {
+            queue.add(neighbors.next());
         }
     }
 
 }
 
 interface IRunner {
-    void run() throws IOException;
+    void run();
 
     void flush();
-}
-
-class Data {
-    Operation op;
-    int operand;
-
-    enum Operation {
-        add, remove, check, toggle, all, empty
-    }
-
-    void set(String input) {
-        String[] parts = input.split(" ");
-        this.op = Operation.valueOf(parts[0]);
-        this.operand = parts.length > 1 ? Integer.parseInt(parts[1]) : -1;
-    }
 }
