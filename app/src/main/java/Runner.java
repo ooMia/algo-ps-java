7a24f0acc468e3f6e2536a37917d83792c9166a1
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-16 15:49:55 +0900

    ✨ Solve : 11047

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,40 +1,29 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.BitSet;
+import java.util.TreeSet;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int nAlphabets = 'z' - 'a' + 1;
-    final int Q;
-    final int[][] cumuls; // cumul[alphabet - 'a'][index]
+    final int N, K;
+    final TreeSet<Integer> divisors = new TreeSet<>((a, b) -> Integer.compare(b, a));
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
 
         try {
-            char[] S = reader.line().toCharArray();
-            cumuls = new int[nAlphabets][S.length];
-            Q = reader.readInts()[0];
-            sb.ensureCapacity(Q * S.length / nAlphabets);
+            var input = reader.readInts();
+            N = input[0];
+            K = input[1];
+            sb.ensureCapacity(20);
 
-            BitSet uniqueChars = new BitSet('z' - 'a' + 1);
-            {
-                char next = S[0];
-                uniqueChars.set(next - 'a');
-                cumuls[next - 'a'][0] = 1;
-            }
-            for (int i = 1; i < S.length; ++i) {
-                char next = S[i];
-                uniqueChars.set(next - 'a');
-                for (int j = uniqueChars.nextSetBit(0); j >= 0; j = uniqueChars.nextSetBit(j + 1)) {
-                    cumuls[j][i] = cumuls[j][i - 1] + (next == j + 'a' ? 1 : 0);
-                }
-            }
+            reader.lines().forEach(line -> {
+                divisors.add(Integer.parseInt(line));
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -52,21 +41,15 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        for (int i = 0; i < Q; ++i) {
-            var input = reader.line().split(" ");
-            char c = input[0].charAt(0);
-            int iFrom = Integer.parseInt(input[1]);
-            int iTo = Integer.parseInt(input[2]);
-            int found = sum(c, iFrom, iTo);
-            sb.append(found).append('\n');
+        int money = K;
+        int count = 0;
+        var it = divisors.iterator();
+        while (it.hasNext()) {
+            int divisor = it.next();
+            count += money / divisor;
+            money %= divisor;
         }
-    }
-
-    private int sum(char c, int iFrom, int iTo) {
-        if (iFrom <= 0) {
-            return cumuls[c - 'a'][iTo];
-        }
-        return cumuls[c - 'a'][iTo] - cumuls[c - 'a'][iFrom - 1];
+        sb.append(count).append('\n');
     }
 }
 
