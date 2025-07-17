dfcee858fd5c03ec9347fcf33358f158c49a843f
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-17 19:18:16 +0900

    ✨ Solve : 1912

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -8,20 +8,16 @@ class Runner implements IRunner {
     final StringBuilder sb = new StringBuilder();
 
     final int N;
-    final int[] values;
-    final int[][] scores;
+    final int[] numbers;
+    final int[] dp;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
-
         try {
-            var input = reader.readInts();
-            N = input[0];
-            values = reader.lines()
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            scores = new int[N][2];
+            N = reader.readInts()[0];
+            numbers = reader.readInts();
+            dp = new int[N];
             sb.ensureCapacity(20);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -40,39 +36,16 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        scores[0][0] = values[0];
-        if (N > 1) {
-            scores[1][0] = values[1];
-            scores[1][1] = values[0] + values[1];
+        // 10 -4 3 1 5 6 -35 12 21 -1
+        // 10 6 9 10 15 16 -19 12 33 32
+
+        int res;
+        res = dp[0] = numbers[0];
+        for (int i = 1; i < N; ++i) {
+            dp[i] = Math.max(dp[i - 1] + numbers[i], numbers[i]);
+            res = Math.max(res, dp[i]);
         }
-
-        // 단순 점화식은
-        // f(x) = max( arr[x] + f(x-1), arr[x] + f(x-2) )
-        // 하지만 연속된 3개의 계단을 밟는 결과가 나와서는 안 됨
-        // 따라서 각 위치에 대한 dp 값은 연속으로 참조한 계단의 수가 포함되어야 함
-
-        for (int i = 0; i < N; i++) {
-            forward(i, 1);
-            forward(i, 2);
-        }
-        sb.append(Math.max(scores[N - 1][0], scores[N - 1][1])).append('\n');
-    }
-
-    void forward(int iFrom, int step) {
-        if (iFrom + step >= N) {
-            return;
-        }
-
-        int next = values[iFrom + step];
-        int prev = 0;
-
-        if (step == 1) {
-            prev = scores[iFrom][0];
-            scores[iFrom + step][1] = Math.max(scores[iFrom + step][1], prev + next);
-        } else if (step == 2) {
-            prev = Math.max(scores[iFrom][0], scores[iFrom][1]);
-            scores[iFrom + step][0] = Math.max(scores[iFrom + step][0], prev + next);
-        }
+        sb.append(res).append('\n');
     }
 }
 
