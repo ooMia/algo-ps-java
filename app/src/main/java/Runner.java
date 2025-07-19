77690122a9c855ee41d76cac8902e3072cd7e17f
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-20 03:38:26 +0900

    ✨ Solve : 17219

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,21 +1,29 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int N;
-    final int[] numbers;
+    final int N, M;
+    final Map<String, String> storage;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            N = reader.readInts()[0];
-            numbers = reader.readInts();
+            var _nm = reader.readInts();
+            N = _nm[0];
+            M = _nm[1];
+            storage = new HashMap<>(N);
+            for (int i = 0; i < N; ++i) {
+                var input = reader.line().split(" ");
+                storage.put(input[0], input[1]);
+            }
             sb.ensureCapacity(20);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -34,11 +42,11 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        int res = 0;
-        for (int k : numbers) {
-            res += Math.min(N, k);
+        for (int i = 0; i < M; ++i) {
+            var key = reader.line();
+            var value = storage.get(key);
+            sb.append(value).append('\n');
         }
-        sb.append(res).append('\n');
     }
 }
 
