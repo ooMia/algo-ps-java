19bc67a31b80afd686b636bf45fd8ba761e93633
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-20 04:57:27 +0900

    ✨ Solve : 10986

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.BitSet;
 
 class Runner implements IRunner {
     final IReader reader;
@@ -10,7 +9,7 @@ class Runner implements IRunner {
     final StringBuilder sb = new StringBuilder();
 
     final int N, M;
-    final Map<String, String> storage;
+    final int[] nums;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
@@ -19,11 +18,7 @@ class Runner implements IRunner {
             var _nm = reader.readInts();
             N = _nm[0];
             M = _nm[1];
-            storage = new HashMap<>(N);
-            for (int i = 0; i < N; ++i) {
-                var input = reader.line().split(" ");
-                storage.put(input[0], input[1]);
-            }
+            nums = reader.readInts();
             sb.ensureCapacity(20);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -42,11 +37,33 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        for (int i = 0; i < M; ++i) {
-            var key = reader.line();
-            var value = storage.get(key);
-            sb.append(value).append('\n');
+        long[] cumuls = new long[N];
+        int[] remains = new int[M];
+        BitSet rKey = new BitSet(M);
+        {
+            cumuls[0] = nums[0] % M;
+            int key = (int) (cumuls[0] % M);
+            remains[key]++;
+            rKey.set(key);
         }
+        for (int i = 1; i < N; ++i) {
+            cumuls[i] = cumuls[i - 1] + nums[i];
+            int key = (int) (cumuls[i] % M);
+            remains[key]++;
+            rKey.set(key);
+        }
+
+        long result = remains[0];
+        for (int i = rKey.nextSetBit(0); i >= 0; i = rKey.nextSetBit(i + 1)) {
+            if (remains[i] >= 2) {
+                result += nC2(remains[i]);
+            }
+        }
+        sb.append(result).append('\n');
+    }
+
+    private long nC2(int n) {
+        return (long) n * (n - 1) / 2;
     }
 }
 
