02f2a275d1ea8604fa9872bdb2dc46572e531707
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-21 23:09:33 +0900

    ✨ Solve : 15654

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,35 +1,29 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int N, C; // N: 아이템 개수, C: 최대 소지 무게
-    final int[] weights;
-    final long[] cumuls;
-    final List<Map<Integer, Integer>> dp; // dp[i][w]
+    final int N, M; // N: number of items, M: number to choose from
+    final int[] numbers;
+    final int[] result; // to store the current permutation
+    final boolean[] visited; // to track used numbers
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            var _xyk = reader.readInts();
-            N = _xyk[0];
-            C = _xyk[1];
-            weights = reader.readInts();
-            cumuls = new long[N];
-            cumuls[0] = weights[0];
-            dp = new java.util.ArrayList<>(N);
-            dp.add(new java.util.HashMap<>());
-            for (int i = 1; i < N; ++i) {
-                dp.add(new java.util.HashMap<>());
-                cumuls[i] = cumuls[i - 1] + weights[i];
-            }
+            var nm = reader.readInts();
+            N = nm[0];
+            M = nm[1];
+            numbers = reader.readInts();
+            Arrays.sort(numbers);
+            result = new int[M];
+            visited = new boolean[N];
             sb.ensureCapacity(20);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -48,52 +42,30 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        // 무게 C에 대해 i-index 이후의 아이템을 선택하는 경우의 수 + 아무것도 선택하지 않는 경우
-        var res = pack(C, 0, N) + 1;
-        sb.append(res).append('\n');
-
-        // for (int i = 0; i < N; ++i) {
-        // System.err.println(Arrays.toString(dp.get(i).entrySet().toArray()));
-        // }
+      generatePermutations(0);
     }
 
-    private int pack(int wLimit, int iFrom, int iTo) {
-        // 선택 불가능
-        if (iTo <= iFrom || wLimit <= 0) {
-            return 0;
+    public void generatePermutations(int depth) {
+        // 베이스 케이스: 순열이 완성된 경우 (길이가 M에 도달)
+        if (depth == M) {
+            for (int val : result) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
         }
 
-        var dp_i = dp.get(iFrom);
-        var w = weights[iFrom];
-
-        // 남은 아이템을 모두 선택할 수 있는 경우
-        var cumul = (iFrom > 0) ? cumuls[iTo - 1] - cumuls[iFrom - 1] : cumuls[iTo - 1];
-        if (cumul <= wLimit) {
-            System.err.println("pack(" + wLimit + ", " + iFrom + ", " + iTo + ")");
-            var res = (1 << (iTo - iFrom)) - 1;
-            dp_i.put(wLimit, res);
-            return res;
+        // 재귀 단계: 사용 가능한 각 숫자에 대해 탐색
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = numbers[i];
+                generatePermutations(depth + 1);
+                visited[i] = false;
+            }
         }
-
-        // 현재 아이템을 선택할 수 없는 경우
-        if (wLimit < w) {
-            var res = pack(wLimit, iFrom + 1, iTo);
-            dp_i.put(wLimit, res);
-            return res;
-        }
-        // 이미 계산된 경우
-        if (dp_i.containsKey(wLimit)) {
-            return dp_i.get(wLimit);
-        }
-        // 계산이 필요한 경우
-        // 1. iFrom-index 아이템을 선택하는 경우
-        var case1 = pack(wLimit - w, iFrom + 1, iTo) + 1;
-        // 2. iFrom-index 아이템을 선택하지 않는 경우
-        var case2 = pack(wLimit, iFrom + 1, iTo);
-        var res = case1 + case2;
-        dp_i.put(wLimit, res);
-        return res;
     }
+
 }
 
 interface IRunner {
