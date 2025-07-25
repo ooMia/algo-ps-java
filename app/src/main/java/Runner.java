aa94a536a671e95dd1d07a8923fb8ffee9a23f8b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-26 02:07:31 +0900

    ✨ Solve : 2747

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -7,17 +7,17 @@ class Runner implements IRunner {
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int N; // N: length of string
-    final String[] tokens;
+    final int N;
+    final int[] fibo;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            tokens = new String[2];
-            tokens[0] = reader.line();
-            tokens[1] = reader.line();
-            N = tokens[0].length();
+            N = reader.readInts()[0];
+            fibo = new int[N + 1];
+            fibo[0] = 0;
+            fibo[1] = 1;
             sb.ensureCapacity(20);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -36,20 +36,10 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        int n = tokens[0].length();
-        int m = tokens[1].length();
-        int[][] dp = new int[n + 1][m + 1];
-
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                if (tokens[0].charAt(i - 1) == tokens[1].charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+        for (int i = 2; i <= N; ++i) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
         }
-        sb.append(dp[n][m]).append('\n');
+        sb.append(fibo[N]).append('\n');
     }
 }
 
