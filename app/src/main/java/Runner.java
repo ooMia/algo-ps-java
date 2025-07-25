de470535796a3bda8a8763f1a266580f42f18c30
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-26 03:16:19 +0900

    ✨ Solve : 2749

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -7,15 +7,17 @@ class Runner implements IRunner {
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int N;
-    final long[] fibo;
+    final long N;
+    final int mod = 1_000_000;
+    final int[] fibo;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            N = reader.readInts()[0];
-            fibo = new long[N + 1];
+            N = Long.parseLong(reader.line().trim());
+            int fiboLength = (int) Math.min(N, 1_500_001L);
+            fibo = new int[fiboLength + 1];
             fibo[0] = 0;
             fibo[1] = 1;
             sb.ensureCapacity(20);
@@ -36,10 +38,22 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        for (int i = 2; i <= N; ++i) {
-            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        if (N == 1) {
+            sb.append(fibo[1]).append('\n');
+            return;
         }
-        sb.append(fibo[N]).append('\n');
+
+        for (int i = 2; i <= N; ++i) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % mod;
+
+            if (fibo[i - 1] == fibo[0] && fibo[i] == fibo[1]) {
+                int cycleLength = i - 1;
+                i = (int) (N % cycleLength);
+                sb.append(fibo[i]).append('\n');
+                return;
+            }
+        }
+        sb.append(fibo[(int) N]).append('\n');
     }
 }
 
