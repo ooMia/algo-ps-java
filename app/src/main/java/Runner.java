db72d941ab377f3a76cc4efb14916fb66305da1b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-27 01:14:27 +0900

    ✨ Solve : 11279

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,21 +1,26 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int A, B;
+    final int N;
+    final PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (o1, o2) -> Integer.compare(o2, o1) // Max-heap
+    );
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             var _ab = reader.readInts();
-            A = _ab[0];
-            B = _ab[1];
+            N = _ab[0];
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -33,35 +38,20 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        Integer res = solve(A, B, 1);
-        res = res == null ? -1 : res;
-        sb.append(res).append('\n');
+        List<Integer> inputs = reader.lines().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        for (int i : inputs) {
+            process(i);
+        }
     }
 
-    private Integer solve(long a, long b, int cnt) {
-        System.err.println("solve(" + a + ", " + b + ", " + cnt + ")");
-        // operation 1. multiply by 2
-        // operation 2. multiply by 10 and add 1
-        if (b < a) {
-            return null;
+    private void process(int o) {
+        if (o == 0) {
+            Integer res = pq.poll();
+            sb.append(res != null ? res : 0).append("\n");
         }
-        if (a == b) {
-            return cnt;
+        else {
+            pq.offer(o);
         }
-        Integer case1 = solve(a * 2, b, cnt + 1);
-        if (case1 == null) {
-            case1 = Integer.MAX_VALUE;
-        }
-        Integer case2 = solve(a * 10 + 1, b, cnt + 1);
-        if (case2 == null) {
-            case2 = Integer.MAX_VALUE;
-        }
-
-        int res = Math.min(case1, case2);
-        if (res == Integer.MAX_VALUE) {
-            return null;
-        }
-        return res;
     }
 
 }
