072ba3c9c6085dcc5fcf74ee7bf30bbfd11b6ffd
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-26 23:55:21 +0900

    ✨ Solve : 16953

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -7,17 +7,15 @@ class Runner implements IRunner {
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int N, K; // N, K: location of A and B; 0 <= N, K <= 10^6
-    final int[] dp;
+    final int A, B;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            var _nk = reader.readInts();
-            N = _nk[0];
-            K = _nk[1];
-            dp = new int[100_001];
+            var _ab = reader.readInts();
+            A = _ab[0];
+            B = _ab[1];
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -35,32 +33,35 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        int res = solve(N, K);
+        Integer res = solve(A, B, 1);
+        res = res == null ? -1 : res;
         sb.append(res).append('\n');
     }
 
-    private int solve(int n, int k) {
-        System.err.println("solve(" + n + ", " + k + ")");
-        // K가 홀수면 -1 +1 구해서 짝수로 만들고
-        // K가 짝수면 N과 가장 가까운 짝수와 비교하고
-        // K가 N의 거듭제곱 꼴이면 바로 반환
-        if (k <= n)
-            return n - k;
-        if (dp[k] != 0)
-            return dp[k];
-        if (n == 0) {
-            return dp[k] = solve(1, k) + 1;
+    private Integer solve(long a, long b, int cnt) {
+        System.err.println("solve(" + a + ", " + b + ", " + cnt + ")");
+        // operation 1. multiply by 2
+        // operation 2. multiply by 10 and add 1
+        if (b < a) {
+            return null;
+        }
+        if (a == b) {
+            return cnt;
+        }
+        Integer case1 = solve(a * 2, b, cnt + 1);
+        if (case1 == null) {
+            case1 = Integer.MAX_VALUE;
+        }
+        Integer case2 = solve(a * 10 + 1, b, cnt + 1);
+        if (case2 == null) {
+            case2 = Integer.MAX_VALUE;
         }
 
-        if (k % 2 == 1) {
-            int case1 = solve(n, k - 1) + 1;
-            int case2 = solve(n, k + 1) + 1;
-            return dp[k] = Math.min(case1, case2);
+        int res = Math.min(case1, case2);
+        if (res == Integer.MAX_VALUE) {
+            return null;
         }
-        while (n < k && k % 2 == 0) {
-            k /= 2;
-        }
-        return dp[k] = solve(n, k);
+        return res;
     }
 
 }
