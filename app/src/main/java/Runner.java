12e294ea7e3641a2c6c99cb1da541da6e1d94dfd
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-29 06:52:32 +0900

    ✨ Solve : 9252

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,38 +1,24 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    char[][] grid = new char[9][9];
-    boolean[][] rowUsed = new boolean[9][10];
-    boolean[][] colUsed = new boolean[9][10];
-    boolean[][] boxUsed = new boolean[9][10];
-    List<int[]> blanks = new ArrayList<>();
+    final int N; // N: length of string
+    final String[] tokens;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            for (int y = 0; y < 9; ++y) {
-                grid[y] = reader.line().toCharArray();
-                for (int x = 0; x < 9; ++x) {
-                    int num = grid[y][x] - '0';
-                    if (num == 0) {
-                        blanks.add(new int[] { y, x });
-                    } else {
-                        int box = (y / 3) * 3 + (x / 3);
-                        rowUsed[y][num] = true;
-                        colUsed[x][num] = true;
-                        boxUsed[box][num] = true;
-                    }
-                }
-            }
+            tokens = new String[2];
+            tokens[0] = reader.line();
+            tokens[1] = reader.line();
+            N = tokens[0].length();
+            sb.ensureCapacity(20);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -49,37 +35,45 @@ class Runner implements IRunner {
     }
 
     @Override
-    public void run() {
-        solve(0);
-        for (int y = 0; y < 9; ++y) {
-            for (int x = 0; x < 9; ++x) {
-                sb.append(grid[y][x]);
-            }
-            sb.append('\n');
-        }
-    }
+    public void run() throws IOException {
+        int n = tokens[0].length();
+        int m = tokens[1].length();
+        int[][] dp = new int[n + 1][m + 1];
 
-    private boolean solve(int idx) {
-        if (idx == blanks.size())
-            return true;
-        int y = blanks.get(idx)[0], x = blanks.get(idx)[1];
-        int box = (y / 3) * 3 + (x / 3);
-        for (int num = 1; num <= 9; ++num) {
-            if (!rowUsed[y][num] && !colUsed[x][num] && !boxUsed[box][num]) {
-                grid[y][x] = (char) ('0' + num);
-                rowUsed[y][num] = colUsed[x][num] = boxUsed[box][num] = true;
-                if (solve(idx + 1))
-                    return true;
-                grid[y][x] = '0';
-                rowUsed[y][num] = colUsed[x][num] = boxUsed[box][num] = false;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (tokens[0].charAt(i - 1) == tokens[1].charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
-        return false;
+
+        var res = dp[n][m];
+        sb.append(res).append('\n');
+        if (res != 0) {
+            int y = n, x = m;
+            StringBuilder lcs = new StringBuilder();
+            while (y > 0 && x > 0) {
+                if (tokens[0].charAt(y - 1) == tokens[1].charAt(x - 1)) {
+                    lcs.append(tokens[0].charAt(y - 1));
+                    --y;
+                    --x;
+                } else if (dp[y - 1][x] >= dp[y][x - 1]) {
+                    --y;
+                } else {
+                    --x;
+                }
+            }
+            lcs.reverse();
+            sb.append(lcs).append('\n');
+        }
     }
 }
 
 interface IRunner {
     void run() throws IOException;
 
-    void flush() throws IOException;
+    void flush();
 }
