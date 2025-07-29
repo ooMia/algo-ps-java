866edaaa44e350659698b6199a4170861dbe434f
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-29 20:08:01 +0900

    ✨ Solve : 프로그래머스/120838

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 class Runner implements IRunner {
     final IReader reader;
@@ -9,13 +8,13 @@ class Runner implements IRunner {
     final StringBuilder sb = new StringBuilder();
 
     final Solution solution = new Solution();
-    final int[] emergency;
+    final String letter;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            emergency = reader.readInts();
+            letter = reader.line();
 
             sb.ensureCapacity(20);
         } catch (IOException e) {
@@ -35,8 +34,8 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        var res = new Solution().solution(emergency);
-        sb.append(Arrays.toString(res)).append('\n');
+        var res = new Solution().solution(letter);
+        sb.append(res).append('\n');
     }
 }
 
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,21 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 class Solution {
-    public int[] solution(int[] emergency) {
-        Map<Integer, Integer> map = new java.util.HashMap<>();
-
-        int[] sorted = emergency.clone();
-        Arrays.sort(sorted);
-        for (int i = 0; i < sorted.length; i++) {
-            map.put(sorted[i], sorted.length - i);
+    public String solution(String letter) {
+        Map<String, String> morse = Map.ofEntries(
+                Map.entry(".-", "a"), Map.entry("-...", "b"), Map.entry("-.-.", "c"), Map.entry("-..", "d"),
+                Map.entry(".", "e"), Map.entry("..-.", "f"), Map.entry("--.", "g"), Map.entry("....", "h"),
+                Map.entry("..", "i"), Map.entry(".---", "j"), Map.entry("-.-", "k"), Map.entry(".-..", "l"),
+                Map.entry("--", "m"), Map.entry("-.", "n"), Map.entry("---", "o"), Map.entry(".--.", "p"),
+                Map.entry("--.-", "q"), Map.entry(".-.", "r"), Map.entry("...", "s"), Map.entry("-", "t"),
+                Map.entry("..-", "u"), Map.entry("...-", "v"), Map.entry(".--", "w"), Map.entry("-..-", "x"),
+                Map.entry("-.--", "y"), Map.entry("--..", "z"));
+                
+        String[] words = letter.split(" ");
+        StringBuilder answer = new StringBuilder();
+        for (String word : words) {
+            String decoded = morse.get(word);
+            if (decoded != null) {
+                answer.append(decoded);
+            }
         }
-        List<Integer> answerList = new ArrayList<>();
-        for (int i = 0; i < emergency.length; i++) {
-            answerList.add(map.get(emergency[i]));
-        }
-        return answerList.stream().mapToInt(i -> i).toArray();
+        return answer.toString();
     }
 }
