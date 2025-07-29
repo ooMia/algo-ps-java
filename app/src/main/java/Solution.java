9407759fce70ac4903519bd11829f0af6d35f382
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-29 20:33:27 +0900

    ✨ Solve : 프로그래머스/86491

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,16 +1,15 @@
-import java.math.BigInteger;
+import java.util.Arrays;
 
 class Solution {
-    public int solution(int balls, int share) {
-        return combination(balls, share);
-    }
-
-    int combination(int n, int m) {
-        BigInteger res = BigInteger.ONE;
-        for (int i = 0; i < m; i++) {
-            res = res.multiply(BigInteger.valueOf(n - i));
-            res = res.divide(BigInteger.valueOf(i + 1));
+    public int solution(int[][] sizes) {
+        int maxWidth = 0, maxHeight = 0;
+        for (int[] size : sizes)
+        {
+            Arrays.sort(size);
+            maxWidth = Math.max(maxWidth, size[0]);
+            maxHeight = Math.max(maxHeight, size[1]);
         }
-        return res.intValue();
+        int answer = maxWidth * maxHeight;
+        return answer;
     }
 }
