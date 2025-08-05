8493b9a8811e2e9919d239407801c095d52e9d39
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-06 03:36:34 +0900

    ✨ Solve : 프로그래머스/120888

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,7 +1,16 @@
-import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 class Solution {
-    public int[] solution(String[] strlist) {
-        return Arrays.stream(strlist).mapToInt(String::length).toArray();
+    public String solution(String my_string) {
+        Set<Character> uniqueChars = new LinkedHashSet<>();
+        for (char c : my_string.toCharArray()) {
+            uniqueChars.add(c);
+        }
+        StringBuilder answer = new StringBuilder();
+        for (char c : uniqueChars) {
+            answer.append(c);
+        }
+        return answer.toString();
     }
 }
