50e6857f93a8f727d695a20ae0a349e203292145
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-06 03:12:01 +0900

    ✨ Solve : 프로그래머스/120854

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,37 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int N) {
-        // 소인수 리스트 반환
-        List<Integer> factors = new ArrayList<>();
-
-        int n = N;
-        if (n % 2 == 0) {
-            factors.add(2);
-            while (n % 2 == 0) {
-                n /= 2;
-            }
-        }
-
-        for (int i = 3; i * i <= N; i += 2) {
-            if (n % i == 0) {
-                factors.add(i);
-                while (n % i == 0) {
-                    n /= i;
-                }
-            }
-            if (n == 1) {
-                break;
-            }
-        }
-
-        if (n > 1) {
-            factors.add(n);
-        }
-
-        return factors.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+    public int[] solution(String[] strlist) {
+        return Arrays.stream(strlist).mapToInt(String::length).toArray();
     }
 }
