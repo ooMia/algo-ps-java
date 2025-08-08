e08e8c8a1ddeeb2612598c56b9a23d07958e5dae
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-08 18:56:32 +0900

    ✨ Solve : 2443

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,31 +1,19 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 class Runner implements IRunner {
     final IReader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int N; // N: 통화 기록의 개수
-    final Map<String, Integer> usage = new HashMap<>(); // 사용자별 통화 시간
+    final int N; // N: nRows
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             this.N = reader.readInts()[0];
-            for (int i = 0; i < N; ++i) {
-                String[] line = reader.line().split(" ");
-                String h = line[0].split(":")[0];
-                String m = line[0].split(":")[1];
-                String user = line[1];
-
-                int time = Integer.parseInt(h) * 60 + Integer.parseInt(m);
-                usage.put(user, usage.getOrDefault(user, 0) + time);
-            }
 
             sb.ensureCapacity(20);
         } catch (IOException e) {
@@ -45,7 +33,7 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        var res = new Solution().solution(usage);
+        var res = new Solution().solution(N);
         sb.append(res);
     }
 }
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
 
     final int baseTime = 100;
@@ -8,38 +5,20 @@ class Solution {
     final int unitTime = 50;
     final int unitFee = 3;
 
-    public String solution(Map<String, Integer> usage) {
+    public String solution(int N) {
         StringBuilder sb = new StringBuilder();
 
-        Map<String, Integer> feeMap = new HashMap<>();
-        for (var entry : usage.entrySet()) {
-            String user = entry.getKey();
-            int time = entry.getValue();
-            int fee = calcFee(time);
-            feeMap.put(user, fee);
-        }
-
-        var l = feeMap.entrySet().stream()
-                .sorted(
-                        (o1, o2) -> {
-                            int cmp = Integer.compare(o2.getValue(), o1.getValue());
-                            return cmp != 0 ? cmp : o1.getKey().compareTo(o2.getKey());
-                        })
-                .collect(java.util.stream.Collectors.toList());
-
-        for (var entry : l) {
-            String user = entry.getKey();
-            int fee = entry.getValue();
-            sb.append(user).append(' ').append(fee).append('\n');
+        for (int row = 1; row <= N; ++row) {
+            // 1. i blanks
+            for (int j = 0; j < row - 1; ++j) {
+                sb.append(" ");
+            }
+            // 2. 2(N-row) + 1 stars
+            for (int j = 0; j < 2 * (N - row) + 1; ++j) {
+                sb.append("*");
+            }
+            sb.append("\n");
         }
         return sb.toString();
-    }
-
-    int calcFee(int _time) {
-        if (_time <= baseTime)
-            return baseFee;
-        int extra = _time - baseTime;
-        int units = (extra + unitTime - 1) / unitTime; // 올림 처리
-        return baseFee + units * unitFee;
     }
 }
