3224cea9884bcdee9bc5219d4004bc4f68f90404
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-10 05:16:17 +0900

    ♻️ Refactor : 프로그래머스/120894

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,35 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
 
     public long solution(String numbers) {
-        Map<String, String> mapper = new HashMap<>();
-        mapper.put("zero", "0");
-        mapper.put("one", "1");
-        mapper.put("two", "2");
-        mapper.put("three", "3");
-        mapper.put("four", "4");
-        mapper.put("five", "5");
-        mapper.put("six", "6");
-        mapper.put("seven", "7");
-        mapper.put("eight", "8");
-        mapper.put("nine", "9");
 
-        StringBuilder sb = new StringBuilder();
-        List<Character> temp = new ArrayList<>();
-        for (char c : numbers.toCharArray()) {
-            temp.add(c);
-            var key = temp.stream().map(String::valueOf).reduce("", String::concat);
-            if (mapper.containsKey(key)) {
-                sb.append(mapper.get(key));
-                temp.clear();
-            }
-            System.err.println(temp.toString());
-            System.err.println(key);
+        String[] numbers_arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        for (int i = 0; i < numbers_arr.length; ++i) {
+            numbers = numbers.replace(numbers_arr[i], String.valueOf(i));
         }
-        return Long.parseLong(sb.toString());
+
+        long answer = Long.parseLong(numbers);
+        return answer;
     }
 }
