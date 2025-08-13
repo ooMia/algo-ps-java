4b1cd9819d8cba15c3389e5978f0e31e8477a939
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-14 01:34:05 +0900

    ✨ Solve : 2630

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -7,13 +7,20 @@ class Runner implements IRunner {
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int T;
+    final int N;
+    final char[][] grid;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            this.T = reader.readInts()[0];
+            this.N = reader.readInts()[0];
+            this.grid = new char[N][N];
+            for (int r = 0; r < N; ++r) {
+                var s = reader.line();
+                for (int i = 0, c = 0; i < s.length(); i += 2)
+                    this.grid[r][c++] = s.charAt(i);
+            }
 
             sb.ensureCapacity(20);
         } catch (IOException e) {
@@ -33,11 +40,9 @@ class Runner implements IRunner {
 
     @Override
     public void run() throws IOException {
-        var sol = new Solution();
-        for (int t = 0; t < T; ++t) {
-            var res = sol.solution(reader.readInts()[0]);
-            sb.append(res).append('\n');
-        }
+        var sol = new Solution(grid);
+        var res = sol.solution();
+        sb.append(res).append('\n');
     }
 }
 
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,24 +1,82 @@
-class Solution {
-    final long[] dp = new long[101];
+import java.util.Collection;
+import java.util.List;
+import java.util.PriorityQueue;
 
-    Solution() {
-        // 0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9
-        dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 1;
-        dp[4] = 2;
-        dp[5] = 2;
-        dp[6] = 3;
-        dp[7] = 4;
-        dp[8] = 5;
-        dp[9] = 7;
-        dp[10] = 9;
-        for (int i = 11; i <= 100; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 5];
-        }
+class Solution {
+
+    final char[][] grid;
+
+    Solution(char[][] grid) {
+        this.grid = grid;
     }
 
-    public long solution(int N) {
-        return dp[N];
+    public String solution() {
+        PriorityQueue<Paper> queue = new PriorityQueue<>();
+        queue.add(new Paper(0, 0, grid.length));
+
+        int nWhite = 0, nBlue = 0;
+        while (!queue.isEmpty()) {
+            Paper paper = queue.poll();
+            int color = paper.color();
+
+            if (color == -1) { // mixed
+                if (paper.size == 2) {
+                    int Y = paper.minY, X = paper.minX;
+                    for (int y = Y; y < Y + 2; ++y) {
+                        for (int x = X; x < X + 2; ++x) {
+                            if (grid[y][x] == '0') {
+                                ++nWhite;
+                            } else {
+                                ++nBlue;
+                            }
+                        }
+                    }
+                } else
+                    queue.addAll(paper.split());
+            } else if (color == 0) {
+                ++nWhite;
+            } else if (color == 1) {
+                ++nBlue;
+            }
+        }
+        return String.format("%d\n%d", nWhite, nBlue);
+    }
+
+    class Paper implements Comparable<Paper> {
+        final int minY, maxY, minX, maxX, size;
+
+        Paper(int minY, int minX, int size) {
+            this.minY = minY;
+            this.minX = minX;
+            this.size = size;
+            this.maxY = minY + size - 1;
+            this.maxX = minX + size - 1;
+        }
+
+        int color() {
+            // -1: mixed, 0: white, 1: blue
+            char base = grid[minY][minX];
+            for (int y = minY; y <= maxY; ++y) {
+                for (int x = minX; x <= maxX; ++x) {
+                    if (grid[y][x] != base)
+                        return -1; // mixed
+                }
+            }
+            return base - '0';
+        }
+
+        Collection<Paper> split() {
+            int half = size / 2;
+            return List.of(
+                    new Paper(minY, minX, half),
+                    new Paper(minY, minX + half, half),
+                    new Paper(minY + half, minX, half),
+                    new Paper(minY + half, minX + half, half));
+        }
+
+        @Override
+        public int compareTo(Solution.Paper o) {
+            return Integer.compare(this.size, o.size);
+        }
     }
 }
