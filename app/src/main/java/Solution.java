3633f22a006166ab1fb747f8f9545b3b929d17f2
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-14 14:44:03 +0900

    ✨ Solve : 15624

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -3,15 +3,16 @@ import java.math.BigInteger;
 class Solution {
 
     final int n;
+    final BigInteger mod = BigInteger.valueOf(1_000_000_007);
     final BigInteger[] fibo;
 
     Solution(int n) {
         this.n = n;
-        this.fibo = new BigInteger[n + 1];
+        this.fibo = new BigInteger[n + 2];
         fibo[0] = BigInteger.ZERO;
         fibo[1] = BigInteger.ONE;
         for (int i = 2; i <= n; i++) {
-            fibo[i] = fibo[i - 1].add(fibo[i - 2]);
+            fibo[i] = fibo[i - 1].add(fibo[i - 2]).mod(mod);
         }
     }
 
