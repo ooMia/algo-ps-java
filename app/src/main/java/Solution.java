eb9a5570c3b393184e5ddabfa3bc5763d6410f93
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-15 20:21:07 +0900

    ✨ Solve : 2407

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,5 +1,21 @@
+import java.math.BigInteger;
+
 class Solution {
-    public String solution(int N, int M) {
-        return (N % 3 == 0 || M % 3 == 0) ? "YES" : "NO";
+    public BigInteger solution(int N, int M) {
+        M = Math.min(M, N - M); // nCm == nC(n-m)
+
+        BigInteger numerator = BigInteger.ONE;
+        BigInteger _N = BigInteger.valueOf(N);
+        for (BigInteger n=BigInteger.valueOf(N-M+1); n.compareTo(_N) <= 0; n = n.add(BigInteger.ONE)) {
+            numerator = numerator.multiply(n);
+        }
+
+        BigInteger denominator = BigInteger.ONE;
+        BigInteger _M = BigInteger.valueOf(M);
+        for (BigInteger m = BigInteger.ONE; m.compareTo(_M) <= 0; m = m.add(BigInteger.ONE)) {
+            denominator = denominator.multiply(m);
+        }
+
+        return numerator.divide(denominator);
     }
 }
