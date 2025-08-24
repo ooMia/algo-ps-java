98659d4be5197f82a3d037fc911ff020b31b76c8
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-25 07:53:16 +0900

    ✨ Solve : 15681

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,26 +1,37 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Runner {
     final Reader reader;
     final BufferedWriter bw;
     final StringBuilder sb = new StringBuilder();
 
-    final int nRows, nCols;
-    final char[][] grid;
+    final int nNodes, rootId, nQueries;
+    final List<Set<Integer>> edges;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             var input = reader.readInts();
-            this.nRows = input[0];
-            this.nCols = input[1];
+            this.nNodes = input[0];
+            this.rootId = input[1];
+            this.nQueries = input[2];
 
-            this.grid = new char[nRows][];
-            for (int i = 0; i < nRows; ++i) {
-                this.grid[i] = reader.line().toCharArray();
+            this.edges = new ArrayList<>(nNodes + 1);
+            for (int i = 0; i < nNodes + 1; ++i) {
+                edges.add(new HashSet<>());
+            }
+
+            for (int i = 1; i < nNodes; ++i) {
+                var edge = reader.readInts();
+                edges.get(edge[0]).add(edge[1]);
+                edges.get(edge[1]).add(edge[0]);
             }
 
             sb.ensureCapacity(20);
@@ -39,8 +50,10 @@ class Runner {
     }
 
     public void run() throws IOException {
-        var sol = new Solution(nRows, nCols, grid);
-        var res = sol.solution();
-        sb.append(res).append('\n');
+        var sol = new Solution(nNodes, edges, rootId);
+        for (int i = 0; i < nQueries; ++i) {
+            var res = sol.solution(Integer.parseInt(reader.line()));
+            sb.append(res).append('\n');
+        }
     }
 }
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,105 +1,32 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
-    final int nRows, nCols;
-    final char[][] grid;
+    final List<Set<Integer>> edges;
+    final Set<Integer> visited;
+    final int[] counts;
 
-    final int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-    final int[][] groupId;
-    final int[][] length;
-
-    public Solution(int nRows, int nCols, char[][] grid) {
-        this.nRows = nRows;
-        this.nCols = nCols;
-        this.grid = grid;
-
-        this.groupId = new int[nRows][nCols];
-        this.length = new int[nRows][nCols];
+    Solution(int nNodes, List<Set<Integer>> edges, int rootId) {
+        this.edges = edges;
+        this.visited = new HashSet<>(nNodes);
+        this.counts = new int[nNodes + 1];
+        count(rootId);
     }
 
-    public String solution() {
-        var ones = new ArrayList<Point>();
-
-        // 방문하지 않은 빈 칸 '0'을 기점으로
-        // 방문 가능한 빈 칸을 모두 배열에 추가하고
-        // 동일한 groupId와 배열의 길이 값을 length에 부여하기
-        int id = 1;
-        for (int y = 0; y < nRows; ++y)
-            for (int x = 0; x < nCols; ++x)
-                if (grid[y][x] == '1')
-                    ones.add(new Point(y, x));
-                else if (groupId[y][x] == 0)
-                    init(new Point(y, x), id++);
-
-        // 모든 벽 '1'을 기점으로
-        // 4개의 방향에 존재하는 length 값을 모두 더한 값에 다시 1을 더하고 mod 10
-        // 각 방향의 length 값이 동일한 groupId로부터 얻어지지 않도록 주의
-        for (var p : ones) {
-            var set = new HashMap<Integer, Integer>();
-            for (var dir : directions) {
-                int newY = p.y + dir[0], newX = p.x + dir[1];
-                if (newY < 0 || newY >= nRows || newX < 0 || newX >= nCols)
-                    continue;
-                set.put(groupId[newY][newX], length[newY][newX]);
-            }
-
-            int sum = 0;
-            for (int value : set.values())
-                sum += value;
-            grid[p.y][p.x] = (char) (((sum + 1) % 10) + '0');
+    private void count(int rootId) {
+        var children = edges.get(rootId);
+        counts[rootId] = 1;
+        visited.add(rootId);
+        for (var child : children) {
+            if (visited.contains(child))
+                continue;
+            count(child);
+            counts[rootId] += counts[child];
         }
-
-        // 문자열을 이어붙여 반환
-        var sb = new StringBuilder(nRows * (nCols + 1));
-        for (int y = 0; y < nRows; ++y)
-            sb.append(grid[y]).append('\n');
-        return sb.toString();
     }
 
-    void init(Point startPoint, int id) {
-        // 시작점을 기준으로 4개의 방향을 탐색하며 벽이 아닌 빈 칸을 모두 배열에 추가
-        // 방문 여부는 id 부여 여부로 판단
-        // 모든 탐색이 종료된 이후에는 전체 Point를 순회하며 length 초기화
-        var visited = new ArrayList<Point>();
-
-        var q = new LinkedList<Point>();
-        q.add(startPoint);
-        assignId(visited, startPoint, id);
-
-        while (!q.isEmpty()) {
-            var curr = q.poll();
-
-            for (var dir : directions) {
-                int newY = curr.y + dir[0], newX = curr.x + dir[1];
-                if (newY < 0 || newY >= nRows || newX < 0 || newX >= nCols)
-                    continue;
-                if (grid[newY][newX] == '1' || groupId[newY][newX] != 0)
-                    continue;
-
-                var p = new Point(newY, newX);
-                q.add(p);
-                assignId(visited, p, id);
-            }
-        }
-
-        for (var p : visited)
-            length[p.y][p.x] = visited.size();
-    }
-
-    void assignId(List<Point> l, Point p, int id) {
-        l.add(p);
-        groupId[p.y][p.x] = id;
-    }
-
-    class Point {
-        final int y, x;
-
-        Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
+    public int solution(int queryId) {
+        return counts[queryId];
     }
 }
