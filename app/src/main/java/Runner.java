e7d810a8072b34326e30ac07fa532641b8175345
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-30 23:58:40 +0900

    ✨ Solve : 1799

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -8,18 +8,20 @@ class Runner {
     final BufferedWriter bw;
 
     final int N;
-    final BitSet positions;
+    final BitSet[] positions;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             this.N = Integer.parseInt(br.readLine());
-            this.positions = new BitSet(N * N);
+            this.positions = new BitSet[] { new BitSet(N * N), new BitSet(N * N) };
             for (int y = 0; y < N; ++y) {
                 String line = br.readLine();
-                for (int x = 0; x < N * 2; x += 2)
-                    positions.set(y * N + x / 2, (line.charAt(x) == '1'));
+                for (int x = 0; x < N * 2; x += 2) {
+                    var idx = y * N + x / 2;
+                    positions[(y + x / 2) % 2].set(idx, (line.charAt(x) == '1'));
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -36,8 +38,10 @@ class Runner {
     }
 
     public void run() throws IOException {
-        var sol = new Solution(N, positions);
-        var res = sol.solution();
+        int res = 0;
+        for (var ps : positions) {
+            res += new Solution(N, ps).solution();
+        }
         bw.write(String.valueOf(res));
     }
 }
