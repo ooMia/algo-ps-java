d67fb85d71b787f890a5c714aa2dc426df0773e9
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-31 23:38:53 +0900

    ✨ Solve : 25644

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,28 +1,20 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.BitSet;
 
 class Runner {
     final Reader reader;
     final BufferedWriter bw;
 
     final int N;
-    final BitSet[] positions;
+    final int[] prices;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             this.N = Integer.parseInt(br.readLine());
-            this.positions = new BitSet[] { new BitSet(N * N), new BitSet(N * N) };
-            for (int y = 0; y < N; ++y) {
-                String line = br.readLine();
-                for (int x = 0; x < N * 2; x += 2) {
-                    var idx = y * N + x / 2;
-                    positions[(y + x / 2) % 2].set(idx, (line.charAt(x) == '1'));
-                }
-            }
+            this.prices = reader.readInts();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -38,10 +30,7 @@ class Runner {
     }
 
     public void run() throws IOException {
-        int res = 0;
-        for (var ps : positions) {
-            res += new Solution(N, ps).solution();
-        }
+        var res = new Solution(N, prices).solution();
         bw.write(String.valueOf(res));
     }
 }
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,78 +1,28 @@
-import java.util.BitSet;
-
 class Solution {
     final int N;
-    final BitSet positions;
+    final int[] prices;
 
-    final int[] dy = { -1, -1, 1, 1 };
-    final int[] dx = { -1, 1, -1, 1 };
-
-    Solution(int N, BitSet positions) {
+    Solution(int N, int[] prices) {
         this.N = N;
-        this.positions = positions;
+        this.prices = prices;
     }
 
     public int solution() {
-        if (N == 1)
-            return positions.get(0) ? 1 : 0;
-        int maxBishop = 0, i = positions.nextSetBit(0);
-        while (i >= 0) {
-            int res = dfs(i, 1);
-            maxBishop = Math.max(maxBishop, res);
-            i = positions.nextSetBit(i + 1);
+        int[] minValue = new int[N];
+        int[] maxValue = new int[N];
+
+        minValue[0] = prices[0];
+        for (int i = 1; i < N; ++i)
+            minValue[i] = Math.min(minValue[i - 1], prices[i]);
+
+        maxValue[N - 1] = prices[N - 1];
+        for (int i = N - 2; i >= 0; --i)
+            maxValue[i] = Math.max(maxValue[i + 1], prices[i]);
+
+        int maxProfit = 0;
+        for (int i = 0; i < N; ++i) {
+            maxProfit = Math.max(maxProfit, maxValue[i] - minValue[i]);
         }
-        return maxBishop;
-    }
-
-    int dfs(int idx, final int depth) {
-        if (idx < 0 || idx >= N * N)
-            return depth;
-
-        var removed = putBishop(idx);
-
-        int i = positions.nextSetBit(idx + 1);
-        int maxDepth = depth;
-        while (i >= 0) {
-            var removed2 = putBishop(i);
-            int res = dfs(i, depth + 1);
-            resetBoard(removed2);
-
-            maxDepth = Math.max(maxDepth, res);
-            i = positions.nextSetBit(i + 1);
-        }
-
-        resetBoard(removed);
-
-        return maxDepth;
-    }
-
-    BitSet putBishop(int pos) {
-        BitSet removed = new BitSet(N * N);
-        removed.set(pos);
-
-        int row = pos / N;
-        int col = pos % N;
-
-        // 각 대각선 방향으로 탐색
-        for (int dir = 0; dir < 4; dir++) {
-            int r = row + dy[dir];
-            int c = col + dx[dir];
-
-            while (r >= 0 && r < N && c >= 0 && c < N) {
-                int idx = r * N + c;
-                if (positions.get(idx)) {
-                    removed.set(idx);
-                }
-                r += dy[dir];
-                c += dx[dir];
-            }
-        }
-
-        positions.andNot(removed); // clear positions
-        return removed;
-    }
-
-    void resetBoard(BitSet removed) {
-        positions.or(removed);
+        return maxProfit;
     }
 }
