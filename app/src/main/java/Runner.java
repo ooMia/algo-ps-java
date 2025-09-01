f25e67267d0300b6cc8737e9066a8904df178a4b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-02 00:59:44 +0900

    ✨ Solve : 31882

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,35 +1,20 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 class Runner {
     final Reader reader;
     final BufferedWriter bw;
 
     final int N;
-    final Map<String, Integer> users;
+    final String line;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             this.N = Integer.parseInt(br.readLine());
-            this.users = new HashMap<>(N, 1);
-            for (int n = 0; n < N; ++n) {
-                var name = br.readLine();
-                users.put(name, users.getOrDefault(name, 0) + 1);
-            }
-            for (int n = 0; n < N - 1; ++n) {
-                var name = br.readLine();
-                var v = users.get(name);
-                if (v == 1) {
-                    users.remove(name);
-                } else {
-                    users.put(name, v - 1);
-                }
-            }
+            this.line = br.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -45,7 +30,7 @@ class Runner {
     }
 
     public void run() throws IOException {
-        var res = new Solution(N, users).solution();
+        var res = new Solution(N, line).solution();
         bw.write(String.valueOf(res));
     }
 }
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,15 +1,39 @@
-import java.util.Map;
+import java.math.BigInteger;
 
 class Solution {
     final int N;
-    final Map<String, Integer> users;
+    final String line;
 
-    Solution(int N, Map<String, Integer> users) {
+    Solution(int N, String line) {
         this.N = N;
-        this.users = users;
+        this.line = line;
     }
 
-    public String solution() {
-        return users.keySet().iterator().next();
+    public BigInteger solution() {
+        BigInteger res = BigInteger.ZERO;
+        int iStart = 0;
+        while (iStart < N) {
+            // find first '2'
+            while (iStart < N && line.charAt(iStart) != '2')
+                ++iStart;
+            // find local last '2'
+            int iEnd = iStart;
+            while (iEnd < N && line.charAt(iEnd) == '2')
+                ++iEnd;
+            res = res.add(score(iStart, iEnd));
+            iStart = iEnd + 1;
+        }
+        return res;
+    }
+
+    BigInteger score(int iStart, int iEnd) {
+        // [iStart, iEnd)
+        BigInteger res = BigInteger.ZERO;
+        int len = iEnd - iStart;
+        for (int score = 1; len > 0; ++score, --len) {
+            var tmp = BigInteger.valueOf(len).multiply(BigInteger.valueOf(score));
+            res = res.add(tmp);
+        }
+        return res;
     }
 }
