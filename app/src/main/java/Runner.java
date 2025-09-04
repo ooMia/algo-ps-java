64a6f5f5094fcf91f17e40d303f66984635fb92e
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-04 16:45:02 +0900

    ✨ Solve : 22864

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -6,13 +6,17 @@ class Runner {
     final Reader reader;
     final BufferedWriter bw;
 
-    final char letter;
+    final int A, B, C, M; // A: 피로도 증가량, B: 처리량, C: 피로도 감소량, M: 최대 피로도
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            this.letter = br.readLine().charAt(0);
+            var input = reader.readInts();
+            this.A = input[0];
+            this.B = input[1];
+            this.C = input[2];
+            this.M = input[3];
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -28,7 +32,7 @@ class Runner {
     }
 
     public void run() throws IOException {
-        var res = new Solution().solution(letter);
+        var res = new Solution().solution(A, B, C, M);
         bw.write(String.valueOf(res));
     }
 }
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,5 +1,23 @@
 class Solution {
-    public int solution(char letter) {
-        return Math.abs('I' - letter) + 84;
+    /**
+     * @param A 피로도 증가량
+     * @param B 처리량
+     * @param C 피로도 감소량
+     * @param M 최대 피로도
+     * @return 피로도 M을 넘지 않고 24시간 내에 처리할 수 있는 최대 처리량
+     */
+    public int solution(int A, int B, int C, int M) {
+        int time, fatigue, workDone;
+        time = fatigue = workDone = 0;
+        while (time < 24) {
+            if (fatigue + A > M) {
+                fatigue = Math.max(0, fatigue - C);
+            } else {
+                fatigue += A;
+                workDone += B;
+            }
+            ++time;
+        }
+        return workDone;
     }
 }
