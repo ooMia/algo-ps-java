3cf66bd01728e2a47ecbe400917ccef0e373d648
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-06 18:06:35 +0900

    ✨ Solve : 2090

--- temp.py
+++ temp.py
@@ -1,8 +1,12 @@
-h, w = 1000, 1000
-k = 10
-p = 8
-print(h, w, k)
-for r in range(h):
-    print(''.join(str(1 - (
-        c == 0 or r == h - 1 or r % p == 0
-    )) for c in range(w)))
+from fractions import Fraction
+
+N = int(input().strip())
+A = list(map(int, input().split()))
+
+# 조화평균 = 1 / sum(1/a)
+s = sum(Fraction(1, a) for a in A)
+h = Fraction(1, 1) / s
+
+num, den = h.numerator, h.denominator
+# 무조건 분수 형태
+print(f"{num}/{den}")
