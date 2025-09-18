6d935d0a16059b6767abcb3ee7c517c72f3644a3
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-18 21:57:24 +0900

    💡 Comment : add explanations for 17626 / 1699

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -8,6 +8,8 @@ class Solution {
     }
 
     int solution() {
+        // dp[i] = i를 제곱수의 합으로 나타낼 때, 필요한 최소 항의 개수
+        // [1, n] 구간의 모든 자연수 i와 i 이하의 제곱수 j*j에 대해 dp[i - j*j] + 1의 최솟값으로 갱신
         for (int i = 1; i <= n; ++i) {
             int ans = Integer.MAX_VALUE;
             for (int j = 1; j * j <= i; ++j) {
