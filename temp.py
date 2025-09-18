1b23f74e16631d94836eefbd2a23a4e16b8966ac
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-18 21:26:17 +0900

    ✨ Solve : 2752

--- temp.py
+++ temp.py
@@ -1,11 +1,3 @@
-from fractions import Fraction
-
-N = int(input().strip())
-A = list(map(int, input().split()))
-
-# 조화평균 = 1 / sum(1/a)
-s = sum(Fraction(1, a) for a in A)
-h = Fraction(1, 1) / s
-
-num, den = h.numerator, h.denominator
-print(f"{num}/{den}")
+numbers = map(int, input().split())
+res = str.join(" ", map(str, sorted(numbers)))
+print(res)
