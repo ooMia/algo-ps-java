a46435c54758b541671820f036bf56fd42e138ba
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-26 19:01:45 +0900

    ✨ Solve : 8741

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,19 +1,18 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.stream.Stream;
 
 class Runner {
     final Reader reader;
     final BufferedWriter bw;
 
-    final Stream<String> S;
+    final int k;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
-            S = br.lines();
+            k = Integer.parseInt(br.readLine());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -30,11 +29,8 @@ class Runner {
 
     public void run() throws IOException {
         var sol = new Solution();
-        S.forEach(s -> {
-            if ("*".equals(s)) return;
-            var res = sol.solution(s);
-            _write(res);
-        });
+        var res = sol.solution(k);
+        _write(res);
     }
 
     private void _write(Object o) {
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,17 +1,11 @@
-import java.util.BitSet;
-
 class Solution {
-    String solution(String S) {
-        return isPangram(S) ? "Y\n" : "N\n";
-    }
-
-    boolean isPangram(String S) {
-        int nAlphas = 'z' - 'a' + 1;
-        BitSet seen = new BitSet(nAlphas);
-        for (char c : S.toCharArray()) {
-            if (' ' == c) continue;
-            seen.set(c - 'a');
-        }
-        return seen.cardinality() == nAlphas;
+    String solution(int k) {
+        // S = (2^k - 1) * (2^k) / 2
+        //   = (2^k - 1) * 2^(k-1)
+        //   = (2^k - 1) << (k-1)
+        var sb = new StringBuilder();
+        for (int i = 0; i < k; ++i) sb.append('1');
+        for (int i = 0; i < k - 1; ++i) sb.append('0');
+        return sb.toString();
     }
 }
