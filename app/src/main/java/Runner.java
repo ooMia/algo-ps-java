840cf53709d22c2bc2146c4ada86927d76589ac8
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-30 07:59:08 +0900

    ✨ Solve : 28356

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -1,36 +1,20 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 class Runner {
     final Reader reader;
     final BufferedWriter bw;
 
-    final int N, M; // N: 물품의 개수, M: 거래 내역의 개수
-    final Map<String, Integer> itemPrices;
-    final List<Transaction> transactions;
+    final int nRows, nCols;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.reader = new Reader(br);
         this.bw = bw;
         try {
             var line = reader.readInts();
-            N = line[0];
-            M = line[1];
-
-            itemPrices = new java.util.HashMap<>(N);
-            for (int n = 0; n < N; ++n) {
-                var item = br.readLine().split(" ");
-                itemPrices.put(item[0], Integer.parseInt(item[1]));
-            }
-
-            transactions = new java.util.ArrayList<>(M);
-            for (int m = 0; m < M; ++m) {
-                var transaction = br.readLine().split(" ");
-                transactions.add(new Transaction(transaction[0], Integer.parseInt(transaction[1])));
-            }
+            nRows = line[0];
+            nCols = line[1];
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -50,7 +34,7 @@ class Runner {
 
     public void run() throws IOException {
         var sol = new Solution();
-        var res = sol.solution(itemPrices, transactions);
+        var res = sol.solution(nRows, nCols);
         _write(res);
     }
 
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,18 +1,56 @@
-import java.util.List;
-import java.util.Map;
-
 class Solution {
+    public String solution(int nRows, int nCols) {
+        int K = 0;
+        int[][] grid = new int[nRows][nCols];
+        while (fillGrid(++K, grid));
+        return toResult(K - 1, grid);
+    }
 
-    final private double upperBoundRate = 1.05;
+    private boolean fillGrid(int K, int[][] grid) {
+        Point start = findNextZero(grid);
+        if (start == null) return false;
 
-    int solution(Map<String, Integer> itemPrices, List<Transaction> transactions) {
-        int exceedCount = 0;
-        for (var entry : transactions) {
-            int originalPrice = itemPrices.get(entry.itemName);
-            double limit = originalPrice * upperBoundRate;
-
-            if (entry.tradedPrice > limit) ++exceedCount;
+        // 0인 값을 발견하면 해당 위치를 기준으로 가로/세로 2칸씩 건너뛰며 K로 채움
+        for (int i = start.r; i < grid.length; i += 2) {
+            for (int j = start.c; j < grid[0].length; j += 2) {
+                grid[i][j] = K;
+            }
         }
-        return exceedCount;
+        return true;
+    }
+
+    class Point {
+        int r, c;
+
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    private Point findNextZero(int[][] grid) {
+        int nRows = Math.min(2, grid.length);
+        int nCols = Math.min(2, grid[0].length);
+        for (int i = 0; i < nRows; ++i) {
+            for (int j = 0; j < nCols; ++j) {
+                if (grid[i][j] == 0) {
+                    return new Point(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    private String toResult(int K, int[][] grid) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(K).append('\n');
+        for (int[] row : grid) {
+            sb.append(row[0]);
+            for (int j = 1; j < row.length; ++j) {
+                sb.append(' ').append(row[j]);
+            }
+            sb.append('\n');
+        }
+        return sb.toString().trim();
     }
 }
