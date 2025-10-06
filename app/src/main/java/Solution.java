4b1986f21ba1fb77921970071ab8b8c4fb1dc608
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-06 11:13:28 +0900

    ✨ Solve : 23305

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,69 +1,26 @@
-import java.util.Collections;
-import java.util.LinkedList;
-
 class Solution {
 
+    final static int maxClassNumber = 1_000_000;
+    final static int[] have = new int[maxClassNumber + 1];  // have[i] = 수업 i를 가진 학생 수
+    final static int[] want = new int[maxClassNumber + 1];  // want[i] = 수업 i를 원하는 학생 수
+
+    // N: 학생 수
+    // cur: i번 학생의 현재 수업 배열
+    // seek: i번 학생의 원하는 수업 배열
     public int solution(int N, int[] cur, int[] seek) {
-        LinkedList<Student> students = new LinkedList<>();
+        // 각 수업별로 현재 가진 학생 수와 원하는 학생 수 계산
         for (int i = 0; i < N; ++i) {
-            students.add(new Student(cur[i], seek[i]));
-        }
-        Collections.sort(students);
-        System.err.println(students);
-
-        int count = N;
-        while (!students.isEmpty()) {
-            var iter = students.iterator();
-            var student = iter.next();
-            if (student.isDone()) {
-                --count;
-                continue;
-            }
-
-            var idx = Collections.binarySearch(students, new Student(student.seek, student.have));
-            if (idx >= 0) {
-                // found exact match
-                count -= 2;
-                students.remove(idx);
-            } else if (-idx - 1 < students.size()) {
-                var candidate = students.get(-idx - 1);
-                if (candidate.have == student.seek) {
-                    var tmp = new Student(student.have, candidate.seek);
-                    students.remove(candidate);
-                    --count;
-
-                    // put tmp back to list
-                    var insertIdx = Collections.binarySearch(students, tmp);
-                    if (insertIdx < 0) insertIdx = -insertIdx - 1;
-                    students.add(insertIdx, tmp);
-                }
-            }
-            students.remove(student);
-        }
-        return count;
-    }
-
-    class Student implements Comparable<Student> {
-        int have, seek;
-
-        Student(int have, int seek) {
-            this.have = have;
-            this.seek = seek;
+            ++have[cur[i]];
+            ++want[seek[i]];
         }
 
-        @Override
-        public int compareTo(Student o) {
-            if (this.have != o.have) return Integer.compare(this.have, o.have);
-            return Integer.compare(this.seek, o.seek);
+        // 각 수업별로 교환 가능한 학생 수
+        // ** maxClassId 만큼만 순회하는 건, max 연산 오버헤드 때문에 생각보다 느림
+        int canExchange = 0;
+        for (int i = 1; i <= maxClassNumber; ++i) {
+            canExchange += Math.min(have[i], want[i]);
         }
 
-        boolean isDone() {
-            return have == seek;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("(%d,%d)", have, seek);
-        }
+        return N - canExchange;
     }
 }
