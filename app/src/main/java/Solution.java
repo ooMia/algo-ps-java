b16606404c613a94b4831faeddb208772654b808
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-09 09:21:51 +0900

    ♻️ Refactor : 18249 lazy-eval

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,19 +1,21 @@
 class Solution {
     final int MOD = 1_000_000_007;
     final int[] dp = new int[191_229 + 1];
+    int iLast = 2;
 
     Solution() {
-        dp[1] =1;
-        dp[2] =2;
-        for (int i = 3; i <= 191_229; ++i) {
-            long temp = (long) dp[i - 1] + dp[i - 2];
-            dp[i] = (int) (temp % MOD);
-        }
+        dp[1] = 1;
+        dp[2] = 2;
     }
 
     public String solution(int[] ns) {
         var sb = new StringBuilder();
         for (var n : ns) {
+            while (iLast < n) {
+                ++iLast;
+                long temp = (long) dp[iLast - 1] + dp[iLast - 2];
+                dp[iLast] = (int) (temp % MOD);
+            }
             sb.append(dp[n]).append('\n');
         }
         return sb.toString();
