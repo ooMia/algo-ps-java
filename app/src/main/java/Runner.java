92ef02ca3b8215ee933a4877d6a5037922ffb3d6
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-20 15:34:24 +0900

    feat: solve 28236

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -5,17 +5,22 @@ import java.io.IOException;
 class Runner {
     final BufferedWriter bw;
 
-    final int N, K;
-    final long[] numbers;
+    final int nRows, nCols;
+    final int[][] positions;
 
     Runner(BufferedReader br, BufferedWriter bw) {
         this.bw = bw;
         var reader = new Reader(br);
         try {
             var line = reader.readInts();
-            N = line[0];
-            K = line[1];
-            numbers = reader.readLongs();
+            nRows = line[0];
+            nCols = line[1] + 2;
+
+            var nClasses = line[2];
+            positions = new int[nClasses][];
+            for (int i = 0; i < nClasses; ++i) {
+                positions[i] = reader.readInts();
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -35,7 +40,7 @@ class Runner {
 
     public void run() throws IOException {
         var sol = new Solution();
-        var res = sol.solution(N, K, numbers);
+        var res = sol.solution(nRows, nCols, positions);
         _write(res);
     }
 
--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,20 +1,21 @@
 import java.util.Arrays;
 
 class Solution {
-    public long solution(int nClasses, int nStudents, long[] numbers) {
-        for (int i = 1; i < numbers.length; ++i) {
-            numbers[i] += numbers[i - 1];
+    public int solution(int nRows, int nCols, int[][] positions) {
+        final int iDestRow = 1, iDestCol = nCols - 1;
+        int winnerId = 0, winnerDist = Integer.MAX_VALUE;
+        for (int i = 0; i < positions.length; ++i) {
+            var pos = positions[i];
+            int dist = manhattan(pos[0], pos[1], iDestRow, iDestCol);
+            if (dist < winnerDist) {
+                winnerDist = dist;
+                winnerId = i + 1;
+            }
         }
-        Arrays.sort(numbers);
-        System.err.println(Arrays.toString(numbers));
+        return winnerId;
+    }
 
-        long sum = 0;
-        int i = 0;
-        while (i < nStudents) {
-            sum += numbers[numbers.length - 1 - i];
-            System.err.println(sum);
-            i++;
-        }
-        return sum;
+    private int manhattan(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
     }
 }
